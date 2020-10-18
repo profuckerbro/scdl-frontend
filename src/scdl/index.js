a7c9156859491a0552cba5d386461be0e22c7ea1
Luke Eversfield <lukeversfield111@gmail.com>
2020-10-18 11:47:41 +0200

    initial commit

---
+++ src/scdl/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Button from "@material-ui/core"
+import ProTip from './ProTip';
+
+
+export default function Scdl() {
+  return (
+    <Container maxWidth="sm">
+      <Box my={4}>
+        <Button/>
+      </Box>
+    </Container>
+  );
+}
